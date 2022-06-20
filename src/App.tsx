02c18826d4657b6a24bea341f0e3be14880537b1
Bryan Acuna <bryanacuna@Bryans-MBP.attlocal.net>
2022-06-20 13:54:05 -0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text-dark">
+        Lorem ipsum dolor sit amet,{" "}
+        <span className="alert-danger">consectetur</span> adipisicing elit.
+        Aliquid amet corporis deserunt distinctio eos esse impedit ipsam
+        laboriosam odio optio quis recusandae repellat sequi similique, sunt
+        suscipit temporibus, unde ut.
+      </p>
     </div>
   );
 }
