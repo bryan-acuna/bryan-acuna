c5132d497e58706a582ebf7ada1264e7931b3d6a
Bryan Acuna <bryanacuna@unknown3e94edbb2613.attlocal.net>
2022-06-25 14:33:34 -0500

    About me section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,51 @@
 import "./About.css";
+import image from "../../assets/moose.png";
+import { BsBadgeArFill } from "react-icons/bs";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+import Card from "./Card";
 const About = () => {
   return (
     <section id="about">
-      <h1>About</h1>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={image} alt="About image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <Card
+              icon={<FaAward className="about__icon" />}
+              title="Experience"
+              desc="30 years"
+            />
+            <Card
+              icon={<FiUsers className="about__icon" />}
+              title="Client"
+              desc="30 years"
+            />
+            <Card
+              icon={<VscFolderLibrary className="about__icon" />}
+              title="Projects"
+              desc="30 years"
+            />
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut
+            blanditiis dolore et, expedita explicabo facilis impedit, itaque
+            molestias neque placeat possimus sit sunt totam ut veniam veritatis
+            voluptas voluptatibus!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/components/about/Card.tsx
@@ -0,0 +1,19 @@
+import { IconType } from "react-icons";
+import { ReactElement } from "react";
+
+export interface CardProps {
+  icon: ReactElement<IconType>;
+  title: string;
+  desc: string;
+}
+const Card = ({ icon, title, desc }: CardProps) => {
+  return (
+    <article className="about__card">
+      {icon}
+      <h5>{title}</h5>
+      <small>{desc}</small>
+    </article>
+  );
+};
+
+export default Card;
