060de672ea4d4e726a44b354605978f7de3c441f
Bryan Acuna <bryanacuna@unknown3e94edbb2613.attlocal.net>
2022-06-25 14:37:45 -0500

    About me section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={image} alt="About image" />
+            <img src={image} alt="About" />
           </div>
         </div>
         <div className="about__content">
